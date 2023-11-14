c57f21004dfd311b72acacc6078c3ce7bacff41c
Fazulhaq Amin <amin.jawan@yahoo.com>
2023-11-14 09:22:57 +0430

    some improvements on the code

--- App.js
+++ App.js
@@ -1,25 +1,34 @@
 const Person = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.occupation),
+    React.createElement("h1", { key: 1 }, props.name),
+    React.createElement("p", { key: 2 }, props.occupation),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", { className: "title" }, "React IS rendered"),
-    React.createElement(Person, { name: "Ahmad", occupation: "Teacher" }, null),
+    React.createElement(
+      "h1",
+      { className: "title", key: 1 },
+      "React IS rendered"
+    ),
     React.createElement(
       Person,
-      { name: "Mahmood", occupation: "Carpenter" },
+      { name: "Ahmad", occupation: "Teacher", key: 2 },
       null
     ),
     React.createElement(
       Person,
-      { name: "Hassan", occupation: "Engineer" },
+      { name: "Mahmood", occupation: "Carpenter", key: 3 },
+      null
+    ),
+    React.createElement(
+      Person,
+      { name: "Hassan", occupation: "Engineer", key: 4 },
       null
     ),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(React.createElement(App));
